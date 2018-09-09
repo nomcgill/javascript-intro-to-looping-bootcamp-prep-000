4d31e50bed6bb8a443814b2cb26f156fa044bd57
Nolan McGill <nomcgill@gmail.com>
2018-09-09 21:39:15 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -19,15 +19,16 @@ var i = 0;
  
 function incrementVariable() {
   i = i + 1;
+}
 do {
   console.log("Where's my cheese?");
   incrementVariable();
 } while (i < 5);
-}
 
 function doWhileLoop(array){
  do {
   array.pop()
   } 
   while (array.length > 0 && incrementVariable());
+  return array
 }
