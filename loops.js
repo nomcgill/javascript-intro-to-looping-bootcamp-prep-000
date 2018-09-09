c5e81dcb9e2437c80aae11b7c87288b5691aa0f9
Nolan McGill <nomcgill@gmail.com>
2018-09-09 19:34:53 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,6 +2,6 @@ function forLoop(array){
   for (let i = 1; i < 25; i++) 
   {
     if (i === 1){array.push("I am 1 strange loop.")}
-    else {array.push('I am ${i} strange loops.')}
+    else {array.push("I am ${i} strange loops.")}
 }
   }
