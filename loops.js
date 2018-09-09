6769a00b95abd1d4a42ede6ca0647b1227a92895
Nolan McGill <nomcgill@gmail.com>
2018-09-09 21:28:15 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -15,11 +15,20 @@ function whileLoop(n){
     return 'done'
 }
 
-
+var i = 0;
+ 
+function incrementVariable() {
+  i = i + 1;
+}
+ 
+do {
+  console.log("doo-bee-doo-bee-doo");
+  incrementVariable();
+} while (i < 5);
 
 function doWhileLoop(array){
  do {
   array.shift()
   } 
-  while (array.length > 0);
+  while (array.length > 0 && incrementVariable);
 }
